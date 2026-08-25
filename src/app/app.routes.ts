70de0f0d686a7e29e69aa6eaea2a10eb52dc189b
VINICIUS SOUZA FRANÇA <09345158596@unijorge.com.br>
2026-08-25 20:52:03 -0300

    Ajusta configurações no angular.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Pessoa } from './pessoa/pessoa';
+import { NaoEncontrada } from './nao-encontrada/nao-encontrada';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', redirectTo: 'home',
+    pathMatch: 'full' },
+
+  { path: 'home',   component: Home    },
+  { path: 'pessoa', component: Pessoa  },
+
+  // precisa ser sempre a ÚLTIMA
+  { path: '**', component: NaoEncontrada }];
